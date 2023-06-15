4ccff39e3e863e167b98e01c18d9478430492126
OwenSyKim <owen.sykim@gmail.com>
2023-06-14 17:00:58 -0700

    Add redux and FE UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <h1 className="text-center m-3">Phone Book</h1>
+        <Row>
+          <Col className="border-column">
+            <ContactForm/>
+          </Col>
+          <Col>
+            <ContactList/>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+// 1. UI on the left=  Add name Box and Phone Number, Right = Search box
+// 2. on the List = able to add user name and PhoneNumber
+// 3. on the List = able to see item's quantity
+// 4. client can search the user by the search the name.
